274b65dcf8116eb16080ee67fbe9db2c2605f02e
NezhaWS <landerking@gmail.com>
2015-05-09 12:36:32 +0100

    Serialization changes

--- Nezability.lua
+++ Nezability.lua
@@ -475,7 +475,7 @@ function Nezability:OnITChannelMessage(channel, Msg, strSender)
 	debugprint("Sender validated.")
 	
 	--Handle message
-	tMsg = loadstring(Msg)
+	loadstring("tMsg ="..Msg)()
 	self.message = tMsg
 
 	if not tMsg then
@@ -514,7 +514,7 @@ function Nezability:SendUpdate()
 	
 	self.group[self.playername].spells = update.spells
 	self:UpdateTotalIA(self.playername)
-	updateStr = UpdateToString(update)
+	updateStr = table.tostring(update)
 	if self.channel then
 		self.channel:SendMessage(updateStr)
 		debugprint("Message Sent!")
@@ -694,7 +694,6 @@ function Nezability:OnNezabilityOn()
 	end
 end
 
-
 --Utilities
 function IsOnline(strPlayerName)
 	local player = GameLib.GetPlayerUnit()
@@ -742,28 +741,57 @@ function debugprint(str)
 	end
 end
 
-function UpdateToString(tbl)
-	local s = {"spells = {"}
-	
-	for i=1,#tbl.spells do
-		s[#s+1] = "{"
-		for j=1,#tbl.spells[i] do
-			if j==1 then s[#s+1]= "id = " end
-			if j==2 then s[#s+1]= "ai = " end
-			if j==3 then s[#s+1]= "cd = " end
-			s[#s+1] = tbl.spells[i][j]
-			if j<3 then s[#s+1] = ","end
-		end
-		s[#s+1] = "}"
-		if i<#tbl.spells then 
-			s[#s+1] = ","
-		end
-	end
-	s[#s+1] = "}"
-	s = table.concat(s)
-	r = "{"..s..", type = "..tbl.type..", classId = "..tbl.classId.."}"
-	return r
+function table.val_to_str ( v )
+  if "string" == type( v ) then
+    v = string.gsub( v, "\n", "\\n" )
+    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+      return "'" .. v .. "'"
+    end
+    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+  else
+    return "table" == type( v ) and table.tostring( v ) or
+      tostring( v )
+  end
 end
+
+function table.key_to_str ( k )
+  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+    return k
+  else
+    return "[" .. table.val_to_str( k ) .. "]"
+  end
+end
+
+function table.tostring( tbl )
+  local result, done = {}, {}
+  for k, v in ipairs( tbl ) do
+    table.insert( result, table.val_to_str( v ) )
+    done[ k ] = true
+  end
+  for k, v in pairs( tbl ) do
+    if not done[ k ] then
+      table.insert( result,
+        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+    end
+  end
+  return "{" .. table.concat( result, "," ) .. "}"
+end
+function table.tostring( tbl )
+  local result, done = {}, {}
+  for k, v in ipairs( tbl ) do
+    table.insert( result, table.val_to_str( v ) )
+    done[ k ] = true
+  end
+  for k, v in pairs( tbl ) do
+    if not done[ k ] then
+      table.insert( result,
+        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+    end
+  end
+  return "{" .. table.concat( result, "," ) .. "}"
+end
+
+
 
 
 
